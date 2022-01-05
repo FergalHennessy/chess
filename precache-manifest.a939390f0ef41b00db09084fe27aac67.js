ee2320e643a88b9a540b635616436f64ca2f8c9a
FergalHennessy <nikeswoosh112015@gmail.com>
2022-01-05 00:15:53 -0600

    Updates

---
+++ precache-manifest.a939390f0ef41b00db09084fe27aac67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9f2ea2af4678027647edc785cb2a421",
+    "url": "/chess/index.html"
+  },
+  {
+    "revision": "4df6d4ca1af62d58ac46",
+    "url": "/chess/static/css/main.f7eee2ae.chunk.css"
+  },
+  {
+    "revision": "a345c846bd2eacc8f56d",
+    "url": "/chess/static/js/2.bd5bd5f8.chunk.js"
+  },
+  {
+    "revision": "56f48a7e5d8405edea9b9ac41b9afaf3",
+    "url": "/chess/static/js/2.bd5bd5f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4df6d4ca1af62d58ac46",
+    "url": "/chess/static/js/main.52e76edc.chunk.js"
+  },
+  {
+    "revision": "e92277677683b7af19b7",
+    "url": "/chess/static/js/runtime-main.f35868b6.js"
+  }
+]);
